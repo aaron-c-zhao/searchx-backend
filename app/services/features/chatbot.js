c42892048946a2063eb325a53b1eafa0c25c77b5
aaronzhao <aaron.zhaocr@gmail.com>
2021-06-09 21:09:20 +0200

    remove regex mathing from backend

--- app/services/features/chatbot.js
+++ app/services/features/chatbot.js
@@ -19,19 +19,20 @@ parser message prefix.
 @bot: bot is the recepiant
 none: bot is not the recepiant
 */
-function _parsePrefix(message) {
-    let reg = /^@bot (.*)/i;
-    let content = null;
-    if (reg.test(message.data.text)) {
-        content = message.data.text.match(reg)[1];
-    }
-    return content;
-}
+// function _parsePrefix(message) {
+//     let reg = /^@bot (.*)/i;
+//     let content = null;
+//     if (reg.test(message.data.text)) {
+//         content = message.data.text.match(reg)[1];
+//     }
+//     return content;
+// }
 
 
 
 module.exports.parse = function(sessionId, message) {
-        const messageContent = _parsePrefix(message);
+        // const messageContent = _parsePrefix(message);
+        const messageContent = message.data.text;
         if (messageContent !== null) {
             console.log("Bot receives message: " + messageContent);
             return new Promise((resolve, reject) => {
